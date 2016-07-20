f668d247abd193d1688ca65dc265c2a469778916
Ruben Dröge <rdroge@progress.com>
2016-07-20 18:38:48 +0000

    Changes made by Ruben Dröge (rdroge@progress.com).

--- Rova Kamen/app.js
+++ Rova Kamen/app.js
@@ -14,7 +14,7 @@
             });
         });
     };
-
+    
     if (window.cordova) {
         document.addEventListener('deviceready', function() {
             if (navigator && navigator.splashscreen) {
@@ -40,50 +40,6 @@
         } else {
             return navigator.connection.type !== 'none';
         }
-    };
-
-    app.openLink = function(url) {
-        if (url.substring(0, 4) === 'geo:' && device.platform === 'iOS') {
-            url = 'http://maps.apple.com/?ll=' + url.substring(4, url.length);
-        }
-
-        window.open(url, '_system');
-        if (window.event) {
-            window.event.preventDefault && window.event.preventDefault();
-            window.event.returnValue = false;
-        }
-    };
-
-    app.onShowMore = function() {
-        var navigation_show_more_view = $("#navigation-show-more-view");
-
-        navigation_show_more_view.find("ul").html($("#navigation-container-more").html());
-
-        navigation_show_more_view.find("ul a").each(function(index) {
-            var icon = '<span class="km-icon km-' + $(this).data('icon') + '"></span>',
-                text = '<span class="km-text">' + $(this).text() + '</span>';
-
-            $(this).html(icon + text).addClass('km-listview-link').attr('data-role', 'listview-link').wrap("<li></li>");
-        });
-
-        $("#more-view-back").off("click").on("click", function() {
-            $("#navigation-show-more-view").hide();
-        })
-    };
-
-    app.afterShowMore = function() {
-        var navigation_show_more_view = $("#navigation-show-more-view");
-
-        navigation_show_more_view.find("li").off('click touchend').on('click touchend', function() {
-            navigation_show_more_view.hide();
-            $('.km-tabstrip .km-state-active').removeClass('km-state-active');
-        });
-    };
-
-    app.clickMore = function(e) {
-        app.onShowMore();
-        $("#navigation-show-more-view").show();
-        app.afterShowMore();
     };
 }());
 
